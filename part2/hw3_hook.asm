a4150c0549cdac50f28bd6a6739d3b6ea6bb74df
badichiboy <hod.badichi@campus.technion.ac.il>
2021-06-02 10:47:18 +0000

    end

--- part2/hw3_hook.asm
+++ part2/hw3_hook.asm
@@ -5,7 +5,25 @@ msg: .ascii "This code was hacked by Noa Killa's gang\n"
 endmsg:
 
 .section .text
-hook:
+
+
+
+hook: 
+  lea _start,%rax
+  add $0x1e,%rax
+  movb $0xc3,(%rax)
+  call _start
+	mov $1, %rdi		  #stdout
+	mov $msg , %rsi		  
+	mov $41, %rdx		# msg width
+	mov $1, %rax
+	syscall
+  
+  lea _start,%rax
+  add $0x1d,%rax
+  jmp *%rax
   movq $60, %rax
   movq $0, %rdi
   syscall
+
+	
